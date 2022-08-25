4dc81d2155012ac2918a7b87ed4645a1df2cfe5a
hubinichang <hubc@startsi.cn>
2022-08-25 15:55:36 +0800

    alter

--- src/views/media.vue
+++ src/views/media.vue
@@ -1,16 +1,26 @@
 <template>
   <img style="height: 140px" src="../assets/electron.png" />
-  <video id="video" style="width: 100%" src="/videos/PresidentObama.mp4" loop />
+  <video id="video" style="width: 100%" :src="videoUrl" loop />
 </template>
 
 <script lang="ts">
   import { defineComponent } from 'vue'
+  import { join } from 'path'
+
+  const publicPath = join(
+    __dirname,
+    import.meta.env.PROD ? '../..' : '../../../public'
+  )
+
   export default defineComponent({
     name: 'MediaComp',
     data() {
-      return {}
+      return {
+        videoUrl: publicPath + '/videos/PresidentObama.mp4',
+      }
     },
     mounted() {
+      console.log('publicPath', publicPath)
       const myVideo = document.querySelector('#video') as HTMLVideoElement
       if (myVideo != null) {
         myVideo.play()
